9dec7ec5a2c53b62a2c49525a1a6ff2a4f9f964a
Huynh Tien Phat <2409huynhphat@gmail.com>
2023-05-27 11:30:22 +0700

    UI add activity

--- Budget_Management/app/src/main/java/com/example/budget_management/HomeFragment.java
+++ Budget_Management/app/src/main/java/com/example/budget_management/HomeFragment.java
@@ -45,7 +45,6 @@ public class HomeFragment extends Fragment {
         fragment.setArguments(args);
         return fragment;
     }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -61,4 +60,5 @@ public class HomeFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false);
     }
+
 }
