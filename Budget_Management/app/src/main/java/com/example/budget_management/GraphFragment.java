4e94af9fafc1bba59f9ced380fc19bba0d4411de
Phat723 <21522449@gm.uit.edu.vn>
2023-06-09 22:18:18 +0700

    Change GraphFragment UI

--- Budget_Management/app/src/main/java/com/example/budget_management/GraphFragment.java
+++ Budget_Management/app/src/main/java/com/example/budget_management/GraphFragment.java
@@ -1,11 +1,11 @@
 package com.example.budget_management;
 
 import android.graphics.Color;
-import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -26,13 +26,19 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.text.DateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 public class GraphFragment extends Fragment {
     private FirebaseAuth mAuth;
     private DatabaseReference mIncomeDatabase;
     private DatabaseReference mExpenseDatabase;
     private PieChart pieChart;
+    private Button toDayBtn;
+    private Button thisWeekBtn;
+    private Button thisMonthBtn;
+    private Button thisYearBtn;
     GraphFragment(){
 
     }
@@ -41,13 +47,26 @@ public class GraphFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View myview = inflater.inflate(R.layout.fragment_graph,container,false);
+
         pieChart = myview.findViewById(R.id.pieChart);
+        toDayBtn = myview.findViewById(R.id.today_btn);
+        thisWeekBtn = myview.findViewById(R.id.week_btn);
+        thisMonthBtn = myview.findViewById(R.id.month_btn);
+        thisYearBtn = myview.findViewById(R.id.year_btn);
+
         mAuth=FirebaseAuth.getInstance();
         FirebaseUser mUser=mAuth.getCurrentUser();
         String uid=mUser.getUid();
 
         mIncomeDatabase= FirebaseDatabase.getInstance().getReference().child("IncomeData").child(uid);
         mExpenseDatabase= FirebaseDatabase.getInstance().getReference().child("ExpenseData").child(uid);
+
+        toDayBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
 
         mIncomeDatabase.addValueEventListener(new ValueEventListener() {
             int expense = 0;
@@ -56,28 +75,35 @@ public class GraphFragment extends Fragment {
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 for(DataSnapshot mysnap: snapshot.getChildren()){
                     Data data= mysnap.getValue(Data.class);
-                    income += data.getAmount();
+                    if(data.getDate().equals( DateFormat.getDateInstance().format(new Date()))){
+                        income += data.getAmount();
+                    }
                 }
                 mExpenseDatabase.addValueEventListener(new ValueEventListener() {
                     @Override
                     public void onDataChange(@NonNull DataSnapshot snapshot) {
                         for(DataSnapshot mysnap: snapshot.getChildren()){
                             Data data= mysnap.getValue(Data.class);
-                            expense += data.getAmount();
+                            if(data.getDate().equals( DateFormat.getDateInstance().format(new Date()))){
+                                expense += data.getAmount();
+                            }
                         }
                         ArrayList<PieEntry> entries = new ArrayList<>();
                         entries.add(new PieEntry((float) expense, "Doanh thu"));
                         entries.add(new PieEntry((float) income, "Chi phí"));
 
-                        PieDataSet dataSet = new PieDataSet(entries, "Tổng doanh thu và chi phí");
+                        PieDataSet dataSet = new PieDataSet(entries,"");
 
                         dataSet.setValueFormatter(new PercentFormatter(pieChart));
                         dataSet.setValueTextSize(16f);
 
                         // Thiết lập màu cho các phần tử trong biểu đồ tròn
                         ArrayList<Integer> colors = new ArrayList<>();
-                        colors.add(Color.GREEN);
-                        colors.add(Color.RED);
+
+                        int incomeColor = Color.parseColor("#F91115"); //Green but lighter
+                        int expenseColor = Color.parseColor("#00CC66");//Red but lighter
+                        colors.add(expenseColor);
+                        colors.add(incomeColor);
                         dataSet.setColors(colors);
 
                         // Tạo PieData từ PieDataSet
@@ -91,7 +117,7 @@ public class GraphFragment extends Fragment {
                         pieChart.setHoleColor(Color.TRANSPARENT);
                         pieChart.setTransparentCircleRadius(40f);
                         pieChart.setEntryLabelColor(Color.BLACK);
-                        pieChart.setEntryLabelTextSize(16f);
+                        pieChart.setEntryLabelTextSize(14f);
                         pieChart.animateY(1000, Easing.EaseInOutQuad);
                         pieChart.invalidate();
                     }
